6a089c536d30d0a804c9681087a6b0c45f68bf06
tca85 <thiago.alves.85@gmail.com>
2015-12-17 21:06:17 -0200

    modificações para filtrar ebook

--- src/br/com/caelum/livraria/camel/ConfiguracaoCamel.java
+++ src/br/com/caelum/livraria/camel/ConfiguracaoCamel.java
@@ -4,19 +4,19 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 import org.apache.camel.CamelContext;
-import org.apache.camel.LoggingLevel;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.processor.aggregate.AggregationStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
  * Consumindo mensagens JMS com Apache Camel
  * 
- * Acessar a loja e finalizar uma compra:
- * http://localhost:8081/fj36-livraria
+ * Acessar a loja e finalizar uma compra: http://localhost:8081/fj36-livraria
  * 
- * Log:
- * 19:15:09,541  INFO route1:96 - CAMEL Recebendo a MSG ID:40c7aa6f-a503-11e5-a78f-a5c83f0e15bf
+ * Log: 19:15:09,541 INFO route1:96 - CAMEL Recebendo a MSG
+ * ID:40c7aa6f-a503-11e5-a78f-a5c83f0e15bf
  * 
  * @author tca85
  *
@@ -28,9 +28,12 @@ public class ConfiguracaoCamel {
 	@Autowired
 	private CamelContext context;
 
-	//---------------------------------------------------------------------------------------------
+	// ---------------------------------------------------------------------------------------------
 	/**
 	 * Configura as rotas que serão utilizadas pelo Camel
+	 * 
+	 * Envia somente os detalhes do item de formato E-book 7.21 - Filtro e
+	 * divisão de conteúdo
 	 * 
 	 * @throws Exception
 	 */
@@ -40,15 +43,27 @@ public class ConfiguracaoCamel {
 			
 			@Override
 			public void configure() throws Exception {
-				from("jms:topic:jms/TOPICO.LIVRARIA?username=jms&password=jms2")
-				   .log(LoggingLevel.INFO, "CAMEL Recebendo a MSG ${id}")
-				   .to("jms:queue:jms/FILA.GERADOR?username=jms&password=jms2");
+				  from("jms:topic:jms/TOPICO.LIVRARIA?username=jms&password=jms2").
+				    unmarshal().string().
+				    split().
+				      tokenizeXML("item"). //tira cada item do pedido
+				    log("Qtd de itens: ${property[CamelSplitSize]}").
+				    filter().
+				      xpath("/item/formato[text()='EBOOK']").
+				    log("\n ${id} \n Item ebook: ${body}").
+				    aggregate( //agregando itens Ebooks
+				      simple("${id}"), //agrega tokens da mesma mensagens
+				      new ConcatenaAggregationStrategy()). //concatena apenas
+				    completionTimeout(2000l). //espera 2s
+				    transform(body().prepend("<itens>").append("</itens>")).
+				      log("Itens com EBooks apenas: ${body} ").
+				  to("jms:queue:jms/FILA.GERADOR?username=jms&password=jms2");
 			}
 		});
 		context.start();
 	}
-	
-	//---------------------------------------------------------------------------------------------
+
+	// ---------------------------------------------------------------------------------------------
 	/**
 	 * Pára o CamelContext
 	 * 
@@ -58,5 +73,26 @@ public class ConfiguracaoCamel {
 	void destroy() throws Exception {
 		context.stop();
 	}
-	//---------------------------------------------------------------------------------------------
+
+	// ---------------------------------------------------------------------------------------------
+	/**
+	 * Classe de agregação usada no processo
+	 * 
+	 * https://gist.github.com/paulojribp/bcbd13b3ae9bf7a5fa67
+	 * 
+	 * @author tca85
+	 *
+	 */
+	public class ConcatenaAggregationStrategy implements AggregationStrategy {
+		public Exchange aggregate(Exchange msgAntiga, Exchange msgNova) {
+			if (msgAntiga == null) {
+				return msgNova;
+			}
+			String bodyAntigo = msgAntiga.getIn().getBody(String.class);
+			String bodyNovo = msgNova.getIn().getBody(String.class);
+			msgAntiga.getIn().setBody(bodyAntigo + bodyNovo);
+			return msgAntiga;
+		}
+	}
+	// ---------------------------------------------------------------------------------------------
 }
